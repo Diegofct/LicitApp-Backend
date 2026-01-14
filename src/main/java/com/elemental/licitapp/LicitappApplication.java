01b13dc80c4d0b29811e96fde774708ad26163bb
Diegofct <96diegofct@gmail.com>
2026-01-14 10:00:37 -0500

    Primer commit, se crea el proyecto con sus respectivas dependencias

---
+++ src/main/java/com/elemental/licitapp/LicitappApplication.java
@@ -0,0 +1,13 @@
+package com.elemental.licitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elemental/licitapp/LicitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elemental.licitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
